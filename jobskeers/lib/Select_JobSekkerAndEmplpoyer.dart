cae56f3575776224f8dd28e977099b1ca5f37cce
Mosaidur <asifbhuiyan135@gmail.com>
2023-12-24 13:20:58 +0600

    1st commite

---
+++ jobskeers/lib/Select_JobSekkerAndEmplpoyer.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SelectJobSekkerAndEmplpoyer extends StatefulWidget {
+  const SelectJobSekkerAndEmplpoyer({super.key});
+
+  @override
+  State<SelectJobSekkerAndEmplpoyer> createState() => _SelectJobSekkerAndEmplpoyerState();
+}
+
+class _SelectJobSekkerAndEmplpoyerState extends State<SelectJobSekkerAndEmplpoyer> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
