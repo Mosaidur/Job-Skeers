dc38106c0df026f2f6a2a966962bfe0205321ea3
Mosaidur <asifbhuiyan135@gmail.com>
2023-12-24 22:31:20 +0600

    1st commite

--- jobskeers/lib/SplashScreen.dart
+++ jobskeers/lib/SplashScreen.dart
@@ -13,25 +13,26 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-  get darktheme => null;
 
-  startTimer (){
-    Timer (Duration(seconds: 3), 
-            () => Navigator.push(context, 
-            MaterialPageRoute(builder: (context) => SelectJobSekkerAndEmplpoyer () ),
-            )
-    );
-  }
+  // startTimer (){
+  //   Timer (Duration(seconds: 3),
+  //           () => Navigator.push(context,
+  //           MaterialPageRoute(builder: (context) => SelectJobSekkerAndEmplpoyer () ),
+  //           )
+  //   );
+  // }
 
   @override
 
-  void initState (){
-    super.initState();
-    startTimer();
-  }
+  // void initState (){
+  //   super.initState();
+  //   startTimer();
+  // }
 
   Widget build(BuildContext context) {
     return Scaffold(
+     // backgroundColor: Color(0xffEFF6FE),
+        backgroundColor: Colors.white,
       body: Center(
           child: Padding(
             padding: const EdgeInsets.fromLTRB(20,0,0,0),
@@ -39,21 +40,48 @@ class _SplashScreenState extends State<SplashScreen> {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Image.asset(darktheme?"image/zanbahon_no color_no_bg.png":"image/zanbahon_pest-color_no-bg.png"),
-                  SizedBox(height: 15,),
-                  Text(
-                    "Fastest & Safest",
-                    style: TextStyle(
-                      color: darktheme ? Colors.white: Colors.teal.shade400,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                      // color: darktheme? Colors.white: Colors.teal.shade400,
-                    ),
-                  ),
+                  Row(
+                    //crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        "assets/images/job-seeking.gif",
+                           // "assets/images/job-search.png",
+                      width: 100,
+                      ),
+                      SizedBox(height: 15,),
+                      Text(
+                        "Seeker",
+                        style: TextStyle(
+                          color: Color(0xff3AB4FB),
+                          fontWeight: FontWeight.bold,
+                          fontSize: 40,
+                          // color: darktheme? Colors.white: Colors.teal.shade400,
+                        ),
+                      ),
+                    ],
+                  )
                 ],
               ),
             ),
           )
+      ),
+
+      bottomNavigationBar: Container(
+        width: double.maxFinite,
+        height: 150,
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset("assets/images/cdc_logo.png",height: 80,),
+                Image.asset("assets/images/skilljobs_logo.png",height: 80,)
+              ],
+            ),
+            Image.asset("assets/images/Daffodil-International-University-DIU-logo.png",height: 50,)
+          ],
+        ),
       ),
     );
   }
