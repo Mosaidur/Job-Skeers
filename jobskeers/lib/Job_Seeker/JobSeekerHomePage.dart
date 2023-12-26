d5ddebbad3fe817304fa3662edd24f4ca21b29e0
Mosaidur <asifbhuiyan135@gmail.com>
2023-12-26 17:26:32 +0600

    3rd commite

--- jobskeers/lib/Job_Seeker/JobSeekerHomePage.dart
+++ jobskeers/lib/Job_Seeker/JobSeekerHomePage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class JobSeekerHompage extends StatefulWidget {
   const JobSeekerHompage({super.key});
@@ -10,6 +12,47 @@ class JobSeekerHompage extends StatefulWidget {
 class _JobSeekerHompageState extends State<JobSeekerHompage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          //crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              //crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+
+
+
+                InkWell(
+                  onTap: () async {
+                    final url = "https://pub.dev/documentation/url_launcher/6.2.2/url_launcher/url_launcher-library.html";
+
+                    launchUrl(url as Uri);
+                    //openBrowserURL (url:url, )
+                  },
+                  child: Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.blue,
+                    child: Center(
+                      child: Text(
+                        "Pub.Dev",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 22,
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
