7d1aefbe262495bae49b525c21308c4eb03c1360
Mosaidur <asifbhuiyan135@gmail.com>
2023-12-31 04:26:44 +0600

    4th commite

--- jobskeers/lib/Job_Seeker/Widgets/BottomAppBarWidget.dart
+++ jobskeers/lib/Job_Seeker/Widgets/BottomAppBarWidget.dart
@@ -10,67 +10,77 @@ class BottomAppBarWidget extends StatefulWidget {
 }
 
 class _BottomAppBarWidgetState extends State<BottomAppBarWidget> {
-  int _selectedIndex = 0;
+  int selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
+      showUnselectedLabels: true,
+      type: BottomNavigationBarType.shifting,
       items: [
         BottomNavigationBarItem(
-          icon: InkWell(
-            onTap: (){
-
-              Navigator.push(context, MaterialPageRoute(builder: (context) => JobSeekerHompage() ));
-
-            },
-              child: Icon(Icons.home)
-          ),
+          icon: Icon(Icons.home),
           label: 'Home',
         ),
         BottomNavigationBarItem(
-          icon: InkWell(
-              onTap: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context) => ComingSoon() ));
-              },
-              child: Icon(Icons.school)
-          ),
+          icon: Icon(Icons.school),
           label: 'Learning',
         ),
         BottomNavigationBarItem(
-          icon: InkWell(
-              onTap: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context) => ComingSoon() ));
-              },
-              child: Icon(Icons.apps)
-          ),
+          icon: Icon(Icons.apps),
           label: 'More',
         ),
         BottomNavigationBarItem(
-          icon: InkWell(
-              onTap: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context) => ComingSoon() ));
-              },
-              child: Icon(Icons.work)
-          ),
+          icon: Icon(Icons.work),
           label: 'My Job',
         ),
         BottomNavigationBarItem(
-          icon: InkWell(
-              onTap: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context) => ComingSoon() ));
-              },
-              child: Icon(Icons.person)
-          ),
+          icon: Icon(Icons.person),
           label: 'Profile',
         ),
       ],
-      currentIndex: _selectedIndex,
+      currentIndex: selectedIndex,
       selectedItemColor: Color(0xff03438C),
       unselectedItemColor: Colors.grey,
       onTap: (index) {
         setState(() {
-          _selectedIndex = index;
+          selectedIndex = index;
         });
+
+        // // Handle navigation here based on the tapped index
+        // switch (index) {
+        //   case 0:
+        //     Navigator.push(
+        //       context,
+        //       MaterialPageRoute(builder: (context) => JobSeekerHompage()),
+        //     );
+        //     break;
+        //   case 1:
+        //     Navigator.push(
+        //       context,
+        //       MaterialPageRoute(builder: (context) => ComingSoon()),
+        //     );
+        //     break;
+        //   case 2:
+        //     Navigator.push(
+        //       context,
+        //       MaterialPageRoute(builder: (context) => ComingSoon()),
+        //     );
+        //     break;
+        //   case 3:
+        //     Navigator.push(
+        //       context,
+        //       MaterialPageRoute(builder: (context) => ComingSoon()),
+        //     );
+        //     break;
+        //   case 4:
+        //     Navigator.push(
+        //       context,
+        //       MaterialPageRoute(builder: (context) => ComingSoon()),
+        //     );
+        //     break;
+        // // Handle other cases as needed
+        // }
       },
     );
   }
