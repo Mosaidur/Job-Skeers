a5ae1d7fcd35609582e240456d8192b54d26ff5a
Mosaidur <asifbhuiyan135@gmail.com>
2024-01-18 12:52:01 +0600

    15th commite

--- jobskeers/lib/Job_Seeker/Pages/Profile/Account Settings/account_settings.dart
+++ jobskeers/lib/Job_Seeker/Pages/Profile/Account Settings/account_settings.dart
@@ -10,26 +10,272 @@ class AccountSetting extends StatefulWidget {
 class _AccountSettingState extends State<AccountSetting> {
   @override
   Widget build(BuildContext context) {
+
+    final String changeEmail = " ";
+    final String changephoneNo = " ";
+    final String changepassword = " ";
+
+
     return Scaffold(
       backgroundColor: Colors.blue.shade50,
       appBar: AppBar(
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-
-            Text("Account Setting"),
-
-            InkWell(
-              onTap: (){},
-              child: Icon(Icons.add_comment),
-            )
-          ],
-        ),
+        title: Text("Account Setting"),
         leading: IconButton(
           icon: Icon(Icons.arrow_back_ios),
           onPressed: () {
             Navigator.of(context).pop();
           },
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+
+            // change email
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0, left: 20,right: 20,bottom: 20),
+              child: Container(
+                width: double.maxFinite,
+                // height: 700,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.white,
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 15.0, left: 20,right: 20,bottom: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+
+                      // change email
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+
+                          Text(
+                            "Email Address",
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            ),
+                          ),
+
+                          InkWell(
+                            onTap: (){},
+                            child: Icon(Icons.edit_note_sharp),
+                          )
+
+                        ],
+                      ),
+                      SizedBox(height: 15,),
+
+
+                      Text(
+                        changeEmail,
+                        textAlign: TextAlign.justify,
+                        // softWrap: true,
+                        // maxLines: 15,
+                        // overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 15,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+
+                      SizedBox(height: 15,),
+
+
+                    ],
+
+                  ),
+                ),
+              ),
+            ),
+
+            // change phone number
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0, left: 20,right: 20,bottom: 20),
+              child: Container(
+                width: double.maxFinite,
+                // height: 700,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.white,
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 15.0, left: 20,right: 20,bottom: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+
+                      // change phone  number
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+
+                          Text(
+                            "Phone Number",
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            ),
+                          ),
+
+                          InkWell(
+                            onTap: (){},
+                            child: Icon(Icons.edit_note_sharp),
+                          )
+
+                        ],
+                      ),
+                      SizedBox(height: 15,),
+
+
+                      Text(
+                        changephoneNo,
+                        textAlign: TextAlign.justify,
+                        // softWrap: true,
+                        // maxLines: 15,
+                        // overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 15,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+
+                      SizedBox(height: 15,),
+
+
+                    ],
+
+                  ),
+                ),
+              ),
+            ),
+
+            // change password
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0, left: 20,right: 20,bottom: 20),
+              child: Container(
+                width: double.maxFinite,
+                // height: 700,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.white,
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 15.0, left: 20,right: 20,bottom: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+
+                      // change password
+                      InkWell(
+                        onTap: (){},
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+
+                            Text(
+                              "Change Password",
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                              ),
+                            ),
+
+                            Icon(Icons.arrow_forward_ios)
+
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 20,),
+
+                      // Privacy Policy
+                      InkWell(
+                        onTap: (){},
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+
+                            Text(
+                              "Privacy Policy",
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                              ),
+                            ),
+
+                            Icon(Icons.arrow_forward_ios)
+
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 20,),
+
+
+                      // Terms and Conditions
+                      InkWell(
+                        onTap: (){},
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+
+                            Text(
+                              "Terms and Conditions",
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                              ),
+                            ),
+
+                            Icon(Icons.arrow_forward_ios)
+
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 20,),
+
+                      // About Us
+                      InkWell(
+                        onTap: (){},
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+
+                            Text(
+                              "About Us",
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                              ),
+                            ),
+
+                            Icon(Icons.arrow_forward_ios)
+
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 20,),
+
+
+                    ],
+
+                  ),
+                ),
+              ),
+            ),
+
+
+          ],
         ),
       ),
     );
--- jobskeers/lib/Job_Seeker/Pages/Profile/Account Settings/upload_photo.dart
+++ jobskeers/lib/Job_Seeker/Pages/Profile/Account Settings/upload_photo.dart
@@ -10,6 +10,9 @@ class UploadPhoto extends StatefulWidget {
 class _UploadPhotoState extends State<UploadPhoto> {
   @override
   Widget build(BuildContext context) {
+
+    final String image= "";
+
     return Scaffold(
       backgroundColor: Colors.blue.shade50,
       appBar: AppBar(
@@ -30,6 +33,136 @@ class _UploadPhotoState extends State<UploadPhoto> {
           onPressed: () {
             Navigator.of(context).pop();
           },
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: Container(
+                child: Column(
+                  children: [
+
+                    SizedBox(height: 40,),
+
+                    (image.isEmpty) ?
+                    InkWell(
+                      onTap: (){},
+                        child: Container(
+                          height: 180,
+                            width: 180,
+                            child: Image.asset("assets/icons/man_logo.png")))
+                        :
+                    InkWell(
+                          onTap: (){},
+                          child: Container(
+                            child: Image.asset(image))),
+
+                    SizedBox(height: 20,),
+
+
+
+                    (image.isEmpty) ?
+                    Text(
+                      "You haven't upload photo yet.",
+                      style: TextStyle(
+                        // color: Color(0xff1891D9),
+                        color: Colors.grey,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    )
+                    :
+                    Text(
+                      "You can change or delete your photo.",
+                      style: TextStyle(
+                        // color: Color(0xff1891D9),
+                        color: Colors.grey,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+
+
+                    SizedBox(height: 20,),
+
+
+
+
+                    (image.isEmpty) ?
+                    //upload photo button
+                    InkWell(
+                      onTap: (){
+                        // Navigator.push(context, MaterialPageRoute(builder: (c) => BrowseJobslistPage()));
+                      },
+                      child: Container(
+                        width: MediaQuery.of(context).size.width/1.9,
+                        // height: 40,
+                        padding: EdgeInsets.all(15),
+                        decoration: BoxDecoration(
+                            color:Color(0xff1891D9),
+                            borderRadius: BorderRadius.circular(15)
+                        ),
+                        child:Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            SizedBox(width: 10,),
+                            Icon(Icons.cloud_upload,color: Colors.white,),
+                            SizedBox(width: 20,),
+                            Text(
+                              "Upoald Photo",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(width: 10,),
+                          ],
+                        ),
+                      ),
+                    )
+                        :
+                    //Edit photo button
+                    InkWell(
+                      onTap: (){
+                        // Navigator.push(context, MaterialPageRoute(builder: (c) => BrowseJobslistPage()));
+                      },
+                      child: Container(
+                        width: MediaQuery.of(context).size.width/1.9,
+                        // height: 40,
+                        padding: EdgeInsets.all(15),
+                        decoration: BoxDecoration(
+                            color:Color(0xff1891D9),
+                            borderRadius: BorderRadius.circular(15)
+                        ),
+                        child:Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            SizedBox(width: 10,),
+                            Icon(Icons.edit,color: Colors.white,),
+                            SizedBox(width: 20,),
+                            Text(
+                              "Edit Photo",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(width: 10,),
+                          ],
+                        ),
+                      ),
+                    )
+
+
+                  ],
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );;
